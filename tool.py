6d8102dfd06b78a1e472e04efdb20ceaca0e26d5
Arc7icWolf <162563901+Arc7icWolf@users.noreply.github.com>
2025-02-05 23:57:26 +0100

    Update tool.py
    script completed and ready to be polished, if needed

--- tool.py
+++ tool.py
@@ -7,14 +7,13 @@ import markdown
 from bs4 import BeautifulSoup
 from langdetect import detect_langs, LangDetectException as Lang_e
 import logging
-from winners_list import update_winners_list
 
 
 # logger
 def get_logger():
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.INFO)
-    handler = logging.FileHandler("community_post_checker.log", mode="a")
+    handler = logging.FileHandler("tool.log", mode="a")
     formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
     handler.setFormatter(formatter)
     logger.addHandler(handler)
@@ -33,7 +32,7 @@ def get_response(data, session: requests.Session):
         "https://api.deathwing.me",
         "https://api.hive.blog",
         "https://hive-api.arcange.eu",
-        "https://api.openhive.network"
+        "https://api.openhive.network",
     ]
     for url in urls:
         request = requests.Request("POST", url=url, data=data).prepare()
@@ -100,155 +99,122 @@ def convert_and_count_words(md_text):
     return len(words)
 
 
-    # CHECK REQUIREMENTS OF THE POSTS // API INTERACTION --> YES
+# CHECK REQUIREMENTS OF THE POSTS // API INTERACTION --> YES
 
 
-# Check if target account commented a post in the past 7 days in the target community
-def replies(author, permlink, seven_days, session: requests.Session):
-    # Get replies from target author
+# Get posts for target author
+def posts(author, session: requests.Session):
     data = (
         f'{{"jsonrpc":"2.0", "method":"bridge.get_account_posts", '
-        f'"params":{{"sort":"comments", "account": "{author}", "limit": 100}}, "id":1}}'
+        f'"params":{{"sort":"posts", "account": "{author}", "limit": 20}}, "id":1}}'
+    )
+    posts = get_response(data, session)
+
+    valid_posts = []
+
+    today = datetime.now()
+    seven_days = today - timedelta(days=6, hours=23)
+    less_than_seven_days = True
+    while less_than_seven_days:
+        for post in posts:
+            category = post["category"]
+            created = post["created"]
+
+            if category == "hive-146620":
+                valid_posts.append(post)
+
+            created_formatted = datetime.strptime(created, "%Y-%m-%dT%H:%M:%S")
+            if created_formatted < seven_days:
+                less_than_seven_days = False
+                print(f"No more posts less than seven days older found for {author}")
+                break  # Stop if post is more than 7 days old
+
+    return valid_posts, len(valid_posts)
+
+
+# Get replies for target post from target author
+def replies(author, permlink, session: requests.Session):
+    data = (
+        f'{{"jsonrpc":"2.0", "method":"condenser_api.get_content_replies", '
+        f'"params":["{author}", "{permlink}"], "id":1}}'
+    )
+    replies = get_response(data, session)
+
+    return len(replies)
+
+
+# Get votes for target post from target author
+def votes(author, permlink, session: requests.Session):
+    data = (
+        f'{{"jsonrpc":"2.0", "method":"condenser_api.get_active_votes", '
+        f'"params":["{author}", "{permlink}"], "id":1}}'
     )
 
-    f'{{"jsonrpc":"2.0", "method":"condenser_api.get_content_replies", '
-    f'"params":["{author}", "{permlink}"], "id":1}}'
+    votes = get_response(data, session)
 
-    replies = get_response(data, session)
-    replies_num = 0
-    valid_reply = False
-    for reply in replies:
-        reply_time = reply["created"]
-        reply_time_formatted = datetime.strptime(reply_time, "%Y-%m-%dT%H:%M:%S")
-
-        if reply_time_formatted < seven_days:
-            break
-
-        if "hive-146620" not in reply.get("community", []):
-            continue  # If the comment is not in the target community, skip
-
-        if reply["children"] == 1 and reply["parent_author"] != author:
-            valid_reply = True  # Look for comments to other authors
-
-        replies_num += 1
-
-    return valid_reply, replies_num
-
-
-# Check if target account voted in one of the 3 last polls
-def has_voted_poll(last_polls, author, session: requests.Session):
-    today = datetime.now()
-    three_weeks_ago = today - timedelta(days=21, hours=23)
-    num = -1
-    polls_voted = 0
-    while True:
-        # Get all custom operations from target account, poll votes included
-        data = (
-            f'{{"jsonrpc":"2.0", "method":"condenser_api.get_account_history", '
-            f'"params":["{author}", {num}, 1000, 262144], "id":1}}'
-        )
-        custom_json = get_response(data, session)
-        for op in custom_json:
-            link = op[1]["op"][1]["id"]
-            if link in last_polls:
-                polls_voted += 1
-        timestamp = custom_json[0][1]["timestamp"]
-        timestamp_formatted = datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S")
-        if timestamp_formatted < three_weeks_ago:
-            return polls_voted
-        num = custom_json[0][0]
+    return len(votes)
 
 
 # Found and check eligible posts published in the last 7 days in the target community
 def eligible_posts(session: requests.Session):
-    today = datetime.now()
-    seven_days = today - timedelta(days=6, hours=23)
-
-    less_than_seven_days = True
-
     authors = [
-        "libertycrypto27", 
-        "will91", 
-        "steveguereschi", 
-        "lozio71", 
-        "harbiter", 
-        "arc7icwolf"
+        "libertycrypto27",
+        "will91",
+        "steveguereschi",
+        "lozio71",
+        "harbiter",
+        "arc7icwolf",
     ]
 
+    entries = []
+
     for author in authors:
-        while less_than_seven_days:
-            # Get posts published by the target author
-            data = (
-                f'{{"jsonrpc":"2.0", "method":"bridge.get_account_posts", '
-                f'"params":{{"sort":"posts", "account": "{author}", "limit": 20}}, "id":1}}'
+        valid_posts, total_posts = posts(author, session)
+
+        total_replies = 0
+        total_votes = 0
+        total_words = 0
+
+        for post in valid_posts:
+            body = post["body"]
+            permlink = post["permlink"]
+
+            lang_num = text_language(body)
+            word_count = convert_and_count_words(body)
+            if lang_num == 2:
+                word_count = word_count // 2
+
+            total_words += word_count
+
+            replies_num = replies(author, permlink, session)
+            total_replies += replies_num
+
+            votes_num = votes(author, permlink, session)
+            total_votes += votes_num
+
+        if total_posts != 0:
+            formula = (
+                (total_words / total_posts * 0.8)
+                + (total_replies / total_posts * 0.2)
+                + (total_votes / total_posts * 0.001)
             )
-            posts = get_response(data, session)
-            for post in posts:
-                author = post["author"]
-                category = post["category"]
-                body = post["body"]
-                created = post["created"]
-                permlink = post["permlink"]
-                title = post["title"]
+        else:
+            formula = 0
 
-                if category != "hive-146620":
-                    continue  # Skip post published in a different community
+        result = (
+            f"- **{author}** ha pubblicato {total_posts} post "
+            f"per un totale di {total_words} parole, "
+            f"ottenendo {total_replies} risposte "
+            f"e {total_votes} voti, "
+            f"per un punteggio finale di {formula:.2f} punti"
+        )
 
-                created_formatted = datetime.strptime(created, "%Y-%m-%dT%H:%M:%S")
-                if created_formatted < seven_days:
-                    less_than_seven_days = False
-                    print("No more posts less than seven days older found")
-                    break  # Stop if post is more than 7 days old
-
-                lang_num = text_language(body)
-
-                word_count = convert_and_count_words(body)
-
-                if lang_num == 2:
-                    word_count = word_count // 2
-                    
-                    
-                replies_num = replies(author, permlink, seven_days, session)
-
-                polls_voted = has_voted_poll(last_polls, author, session)
-                if polls_voted == 0:
-                    continue
-
-                author_stats = (
-                    f"- **{author}** made **{replies_num} comments**"
-                    f" and voted in **{polls_voted} polls**"
-                )
-                if author_stats not in authors_stats:
-                    authors_stats.append(author_stats)
-
-                beneficiary = "no"
-                beneficiary_weight_formatted = ""
-                for beneficiary in beneficiaries:
-                    if beneficiary.get("account", []) == "balaenoptera":
-                        beneficiary_weight = beneficiary.get("weight", [])
-                        beneficiary_weight_formatted = f"for {int(beneficiary_weight / 100)}%"
-                        beneficiary = "yes"
-                        break
-
-                message = (
-                    f"{i}) {author} published ['{title}'](https://www.peakd.com/@{author}/{permlink})"
-                    f" ---> balaenoptera as beneficiary? {beneficiary} {beneficiary_weight_formatted}"
-                )
-
-                entries.append(message)
-
-                print(message)
-
-                i += 1
+        entries.append(result)
 
     with open("entries.txt", "w", newline="", encoding="utf-8") as file:
+        entries.sort(key=lambda x: float(x.split()[-2]), reverse=True)
         for entry in entries:
             file.write(f"{entry}\n")
-
-    with open("authors_list.txt", "w", newline="", encoding="utf-8") as file:
-        authors_stats.sort()
-        for author_stats in authors_stats:
-            file.write(f"{author_stats}\n")
 
 
 def main():
@@ -262,12 +228,8 @@ def main():
     except Exception as e:
         logger.error(f"An error occurred: {e}")
 
-    update_winners_list(session)
-
     elapsed_time = time.time() - start
     print(f"Work completed in {elapsed_time:.2f} seconds")
-
-    return True
 
 
 if __name__ == "__main__":
